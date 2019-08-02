91e16bcaa8f3cbda0c18a8684628be8c452a45c3
Siddique Data Engineer <dba.siddique@gmail.com>
2019-08-02 16:52:00 +0500

    Implement data processing pipeline

---
+++ src/main.py
@@ -0,0 +1,16 @@
+"""
+39 Streaming Fraud Detection - Main Implementation
+"""
+
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def main():
+    logger.info("Starting 39-streaming-fraud-detection")
+    # Implementation here
+    pass
+
+if __name__ == "__main__":
+    main()
